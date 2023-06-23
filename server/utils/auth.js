33cb1bfa56ac4bbba9c6643b5bc2bed6899d36c2
stasiaDiamond <nsisco@uw.edu>
2023-06-22 22:34:39 -0700

    heroku please

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,34 +1,32 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+export const authMiddleware = function ({ req }) {
+  let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
 
-    if (!token) {
-      return req;
-    }
-
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-    }
-
+  if (!token) {
     return req;
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  }
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    console.log('Invalid token');
+  }
+
+  return req;
+};
+
+export const signToken = function ({ username, email, _id }) {
+  const payload = { username, email, _id };
+
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
 };

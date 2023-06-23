d5bbb1428811d96abeb6a47650e9c7a898eea4c6
stasiaDiamond <nsisco@uw.edu>
2023-06-22 23:40:05 -0700

    heroku bugs

--- server/server.js
+++ server/server.js
@@ -3,16 +3,20 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
-const {typeDefs, resolvers} = require('./schemas')
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server= new ApolloServer({
+
+const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
-})
+  cacheControl: {
+    defaultMaxAge: 3600, // Set an appropriate max age value
+  },
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,31 +26,20 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.get('/*', function (req, res) {
-//   res.sendFile(
-//     path.join(__dirname, '../client/build/index.html'),
-//     function (err) {
-//       if (err) {
-//         res.status(500).send(err);
-//       }
-//     }
-//     );
-// });
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
-})
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-const startApolloServer = async(typeDefs, resolvers) => {
+const startApolloServer = async () => {
   await server.start();
-  server.applyMiddleware({app})
-
+  server.applyMiddleware({ app });
 
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`Regular API server running on port ${PORT}`);
       console.log(`GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
+    });
+  });
+};
 
-  startApolloServer(typeDefs, resolvers)
+startApolloServer();
